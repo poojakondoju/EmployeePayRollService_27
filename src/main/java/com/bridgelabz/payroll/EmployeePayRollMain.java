6701ae5ae274ef74d5fca69cfedc1ee9f9591de7
poojakondoju <kondojupoojitha612@gmail.com>
2021-10-03 21:26:14 +0530

    Added : Program for the read and write the employee payroll to a console

---
+++ src/main/java/com/bridgelabz/payroll/EmployeePayRollMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.payroll;
+
+public class EmployeePayRollMain {
+    public static void main(String[] args) {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        System.out.println("Welcome to employee payRoll");
+        employeePayrollService.readEmployeeDataFromConsole();
+        employeePayrollService.writeEmployeeDataInConsole();
+    }
+
+}
+
