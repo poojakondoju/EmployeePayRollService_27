c88cd3e967237e2c35ad58b158125803a76acbe8
poojakondoju <kondojupoojitha612@gmail.com>
2021-10-03 21:39:43 +0530

    Added : Program for the demo of file is exit or not crete directory etc,

--- src/main/java/com/bridgelabz/payroll/EmployeePayRollMain.java
+++ src/main/java/com/bridgelabz/payroll/EmployeePayRollMain.java
@@ -3,10 +3,8 @@ package com.bridgelabz.payroll;
 public class EmployeePayRollMain {
     public static void main(String[] args) {
         EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-        System.out.println("Welcome to employee payRoll");
+        System.out.println("Welcolme to employee payRoll");
         employeePayrollService.readEmployeeDataFromConsole();
         employeePayrollService.writeEmployeeDataInConsole();
     }
-
 }
-
--- src/main/java/com/bridgelabz/payroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/payroll/EmployeePayrollService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    private List<EmployeeData> employeePayrollList = new ArrayList<EmployeeData>();
+    private List<EmployeeData> employePayrollList = new ArrayList<EmployeeData>();
     Scanner scanner = new Scanner(System.in);
 
     public void readEmployeeDataFromConsole() {
@@ -15,11 +15,10 @@ public class EmployeePayrollService {
         String Name = scanner.next();
         System.out.println("Enter the salary");
         int salary = scanner.nextInt();
-        employeePayrollList.add(new EmployeeData(id, Name, salary));
+        employePayrollList.add(new EmployeeData(id, Name, salary));
     }
 
-    public void writeEmployeeDataInConsole() //write the employee data in console.
-    {
-        System.out.println("Writing Employee Pay Roll Data \n" + employeePayrollList);
+    public void writeEmployeeDataInConsole() {
+        System.out.println("Writing Employee Pay Roll Data \n" + employePayrollList);
     }
 }
